86833a451a3f5f72aa96fc9f1467b2d9ed68c578
Jyoti Avhad <avhadjyoti09@gmail.com>
2023-01-10 15:58:52 +0530

    working on horizontal scrollview item layout

--- NeoSTORE/app/src/main/java/com/example/neostore/view/fragments/HomeFragment.kt
+++ NeoSTORE/app/src/main/java/com/example/neostore/view/fragments/HomeFragment.kt
@@ -1,14 +1,15 @@
 package com.example.neostore.view.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.neostore.R
 import com.example.neostore.view.RecyclerGridAdapter
+import kotlinx.android.synthetic.main.categories_list_layout.*
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
@@ -30,21 +31,17 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        //product list on home fragment
         var price = 50.2
-        prodItemList.clear()
-
         for (i in 'A'..'Z') {
-//        for (i in 1..50) {
             prodItemList.add(ProductListModel(R.drawable.ic_home_24px, "$i",price.toFloat()))
         }
-//        rv_card_list.setHasFixedSize(true)
         rv_card_list.layoutManager = GridLayoutManager(activity,2 )
-//        rv_card_list.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL,false)
         recyclerGridAdapter = RecyclerGridAdapter(prodItemList)
         rv_card_list.adapter = recyclerGridAdapter
-//        recyclerGridAdapter.notifyDataSetChanged()
 
 
     }
+
 
 }
